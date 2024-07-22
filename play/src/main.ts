be5b5fc4c01dc5374683baa7476d402dadfa3be8
mrceel <mrceel@163.com>
2024-07-22 10:07:47 +0800

    :new: 添加打包配置

--- play/src/main.ts
+++ play/src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import ClUi from 'ceel-ui'
+// import 'ceel-ui/dist/style.css'
 
 createApp(App).use(ClUi).mount('#app')
